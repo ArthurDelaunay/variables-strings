e1207562d2f4b5434cefe2fbfe81d38e29816199
ArthurDelaunay <delaunayfredericarthur@gmail.com>
2022-06-20 14:31:48 +0200

    exercice 1 to 8

---
+++ index.js
@@ -0,0 +1,36 @@
+// 01 - Hello World
+// console.log("Hello World !");
+
+// 02 - String
+// const sentence= "My name is Arthur"
+// console.log(sentence)
+
+// 03 - Concaténation
+// const name= `Arthur`
+// console.log(`Nice to meet you ${name}`)
+
+// 04 - String Lenght
+// const sentence= "I'm a new text !"
+// console.log(sentence.length)
+
+// 05 - Replace
+// const food="croissant is meh"
+// console.log(food.replace("meh","so good"))
+
+// 06 - Up and Down
+// const basic= "This is Cool"
+// const basicUp= basic.toUpperCase()
+// const basicDown= basic.toLowerCase()
+// console.log (basic)
+// console.log (basicUp)
+// console.log (basicDown)
+
+// 07 - Split
+// const word= "banana"
+// const letters= word.split("")
+// console.log(letters)
+
+// 08 - Template
+const age= 31
+const template= `I'm ${age} years old`
+console.log(template)
